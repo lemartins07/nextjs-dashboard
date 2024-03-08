26bb25a0219f784533f25a0fd93536c392bbab8f
Leandro Martins <leandro.martins.85@gmail.com>
2024-03-08 18:00:04 -0300

    Prettier fix

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,24 +1,24 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination'
+import Search from '@/app/ui/search'
+import Table from '@/app/ui/invoices/table'
+import { CreateInvoice } from '@/app/ui/invoices/buttons'
+import { lusitana } from '@/app/ui/fonts'
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
+import { Suspense } from 'react'
+import { fetchInvoicesPages } from '@/app/lib/data'
 
 export default async function Page({
   searchParams,
 }: {
   searchParams?: {
-    query?: string;
-    page?: string;
-  };
+    query?: string
+    page?: string
+  }
 }) {
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+  const query = searchParams?.query || ''
+  const currentPage = Number(searchParams?.page) || 1
 
-  const totalPages = await fetchInvoicesPages(query);
+  const totalPages = await fetchInvoicesPages(query)
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -28,12 +28,12 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
     </div>
-  );
+  )
 }
